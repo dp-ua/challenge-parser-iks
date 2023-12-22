863cfa1fe3c1ffacd4ae61c5907249b5ad37f3e7
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2023-12-22 23:37:08 +0200

    [bot][command] add CommandInterface and commands: start, help

---
+++ src/main/java/com/dp_ua/iksparser/bot/command/CommandInterface.java
@@ -0,0 +1,35 @@
+package com.dp_ua.iksparser.bot.command;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public interface CommandInterface {
+    String command();
+
+    default List<String> altCommands() {
+        return Collections.EMPTY_LIST;
+    }
+
+    default List<String> partOfStringCommands() {
+        return Collections.EMPTY_LIST;
+    }
+
+    default List<String> fullStringCommands() {
+        return Collections.EMPTY_LIST;
+    }
+
+    default List<String> allSimpleCommands() {
+        return Stream
+                .concat(
+                        Stream.of(command()), altCommands().stream())
+                .collect(Collectors.toList());
+    }
+
+    default String logString() {
+        return "[" + getClass().getSimpleName() + "]:"
+                + "{" + command() + "},"
+                + "alt{" + altCommands() + "}";
+    }
+}
---
+++ src/main/java/com/dp_ua/iksparser/bot/command/impl/CommandStart.java
@@ -0,0 +1,23 @@
+package com.dp_ua.iksparser.bot.command.impl;
+
+import com.dp_ua.iksparser.bot.command.CommandInterface;
+import com.dp_ua.iksparser.bot.command.CommandProvider;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@ToString
+@EqualsAndHashCode
+public class CommandStart implements CommandInterface {
+    private final String command = "start";
+    private final boolean isInTextCommand = false;
+    @Autowired
+    CommandProvider commandProvider;
+
+    @Override
+    public String command() {
+        return command;
+    }
+}
