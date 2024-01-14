b3f31a99752addfad31a1e3557d745acb64d0833
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-01-15 01:39:51 +0200

    [structure] add EnableAsync to config

--- src/main/java/com/dp_ua/iksparser/AppConfig.java
+++ src/main/java/com/dp_ua/iksparser/AppConfig.java
@@ -5,12 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
 @Slf4j
+@EnableAsync
 public class AppConfig {
     @Value("${telegram.bot.token}")
     private String botToken;
