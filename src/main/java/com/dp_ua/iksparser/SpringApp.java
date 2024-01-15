bb8cef92c90e101006991109bc6b17d66f25ca21
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-01-15 17:35:31 +0200

    [experiments] new urls for competitions

--- src/main/java/com/dp_ua/iksparser/SpringApp.java
+++ src/main/java/com/dp_ua/iksparser/SpringApp.java
@@ -11,11 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringApp implements CommandLineRunner {
     @Autowired
     private App app;
-    private final String surname = "Решетило";
+    private final static String surname = "Решетило";
     // барьеры
 //    private final String URL = "https://iks.org.ua/competitions1/en/2024.01.12_kyiv/live?s=D6439A95-F3D4-4E71-B1EA-084D7B356947";
     // пантера
-    private final String URL = "https://iks.org.ua/competitions1/en/2024.01.13_kyiv/live?s=0257D292-2228-4316-9141-DF185D18CDCF";
+    private final static String URL = "https://iks.org.ua/competitions1/en/2024.01.13_kyiv/live?s=0257D292-2228-4316-9141-DF185D18CDCF";
 
 
     public static void main(String[] args) {
@@ -23,7 +23,7 @@ public class SpringApp implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         app.start(URL, surname);
     }
 }
