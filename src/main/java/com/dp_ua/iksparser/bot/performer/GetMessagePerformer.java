49c0e307f4b2374d5d8d17c7548c9e7e6cc08eed
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-01-15 02:01:22 +0200

    [structure] implement simple GetMessagePerformer

---
+++ src/main/java/com/dp_ua/iksparser/bot/performer/GetMessagePerformer.java
@@ -0,0 +1,33 @@
+package com.dp_ua.iksparser.bot.performer;
+
+import com.dp_ua.iksparser.bot.command.TextCommandDetectorService;
+import com.dp_ua.iksparser.bot.message.Message;
+import com.dp_ua.iksparser.bot.performer.event.GetMessageEvent;
+import com.dp_ua.iksparser.exeption.NotForMeException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class GetMessagePerformer implements ApplicationListener<GetMessageEvent> {
+    @Autowired
+    TextCommandDetectorService commandDetector;
+
+    @Override
+    public void onApplicationEvent(GetMessageEvent event) {
+        Message message = event.getMessage();
+        log.info("GetMessageEvent: {}", message);
+        try {
+            commandDetector.getParsedCommands(message.getMessageText())
+                    .forEach(command -> command.execute(message));
+        } catch (NotForMeException e) {
+            log.info("NotForMeException: {}", e.getMessage());
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            log.error("Exception: {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
