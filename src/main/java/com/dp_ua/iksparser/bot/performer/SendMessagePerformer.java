012f9fadfef74c07dea2fb7b67a64388bb6496e8
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-01-18 12:50:38 +0200

    [structure] implement SendMessagePerformer and SendMessageEvent

---
+++ src/main/java/com/dp_ua/iksparser/bot/performer/SendMessagePerformer.java
@@ -0,0 +1,49 @@
+package com.dp_ua.iksparser.bot.performer;
+
+import com.dp_ua.iksparser.bot.Bot;
+import com.dp_ua.iksparser.bot.performer.event.SendMessageEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendChatAction;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@Slf4j
+public class SendMessagePerformer implements ApplicationListener<SendMessageEvent> {
+    @Autowired
+    Bot bot;
+
+    @Override
+    @Async
+    public void onApplicationEvent(SendMessageEvent event) {
+        log.info("SendMessageEvent. Type: {}, Message:{}",
+                event.getMsgType(),
+                event.getMessage().toString().replaceAll("\n", " "));
+        Message result = null;
+        try {
+            switch (event.getMsgType()) {
+                case SEND_MESSAGE:
+                    result = bot.execute((SendMessage) event.getMessage());
+                    break;
+                case CHAT_ACTION:
+                    bot.execute((SendChatAction) event.getMessage());
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unknown msgType: " + event.getMsgType());
+            }
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+        if (result != null) {
+            log.info("Message sent. Result: {}", result.toString().replaceAll("\n", " "));
+            // todo тут можно фиксировать результаты отправки сообщений
+            /*   возможно необходимо будет сохранять айди сообщений для последующего удаления/редактирования
+             */
+        }
+    }
+}
---
+++ src/main/java/com/dp_ua/iksparser/bot/performer/event/SendMessageEvent.java
@@ -0,0 +1,21 @@
+package com.dp_ua.iksparser.bot.performer.event;
+
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+public class SendMessageEvent extends ApplicationEvent {
+    @Getter
+    private final Object message;
+    @Getter
+    private final MsgType msgType;
+
+    public SendMessageEvent(Object source, Object message, MsgType msgType) {
+        super(source);
+        this.message = message;
+        this.msgType = msgType;
+    }
+
+    public enum MsgType {
+        SEND_MESSAGE, CHAT_ACTION
+    }
+}
