438fb3f8de83e4e1a24a4a27123ad508f119e0f1
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-01-19 22:32:18 +0200

    [dba] add DomainElement

---
+++ src/main/java/com/dp_ua/iksparser/element/DomainElement.java
@@ -0,0 +1,36 @@
+package com.dp_ua.iksparser.element;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class DomainElement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected Long id;
+
+    @Column(updatable = false)
+    protected String created;
+
+    protected String updated;
+
+    @PrePersist
+    protected void onCreate() {
+        created = LocalDateTime.now().toString();
+        updated = LocalDateTime.now().toString();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updated = LocalDateTime.now().toString();
+    }
+
+    public LocalDateTime getUpdatedTime() {
+        return LocalDateTime.parse(updated);
+    }
+}
