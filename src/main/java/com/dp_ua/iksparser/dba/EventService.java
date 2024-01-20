1e9cf8c4ff1a2cdd67fa78d02060ee9485e67c4a
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-01-20 19:41:02 +0200

    [dba] update repo,entity,service Event

--- src/main/java/com/dp_ua/iksparser/dba/EventService.java
+++ src/main/java/com/dp_ua/iksparser/dba/EventService.java
@@ -1,5 +1,6 @@
 package com.dp_ua.iksparser.dba;
 
+import com.dp_ua.iksparser.element.EventEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,5 +11,9 @@ public class EventService {
     @Autowired
     public EventService(EventRepo repo) {
         this.repo = repo;
+    }
+
+    public EventEntity save(EventEntity event) {
+        return repo.save(event);
     }
 }
--- src/main/java/com/dp_ua/iksparser/element/EventEntity.java
+++ src/main/java/com/dp_ua/iksparser/element/EventEntity.java
@@ -1,30 +1,31 @@
 package com.dp_ua.iksparser.element;
 
 import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@ToString
 @Getter
+@Setter
 @Entity
-@NoArgsConstructor
-@EqualsAndHashCode
 public class EventEntity extends DomainElement {
     private String time;
     private String eventName;
     private String category;
     private String round;
     private String startListUrl;
+    private String ResultUrl;
     @OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
     private List<HeatEntity> heats;
     @ManyToOne
     @JoinColumn(name = "day_id")
     private DayEntity day;
+
+    public EventEntity() {
+        heats = new ArrayList<>();
+    }
 
     public EventEntity(String time, String eventName, String category, String round, String startListUrl) {
         this.time = time;
