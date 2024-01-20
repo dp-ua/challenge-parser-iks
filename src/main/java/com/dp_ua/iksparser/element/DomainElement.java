a1434496f5d1d315b22507f85a7102abe21f1699
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-01-20 19:43:33 +0200

    [dba] add log messages to DomainElement

--- src/main/java/com/dp_ua/iksparser/element/DomainElement.java
+++ src/main/java/com/dp_ua/iksparser/element/DomainElement.java
@@ -3,12 +3,14 @@ package com.dp_ua.iksparser.element;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
+@Slf4j
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class DomainElement {
     @Id
@@ -24,11 +26,13 @@ public class DomainElement {
     protected void onCreate() {
         created = LocalDateTime.now().toString();
         updated = LocalDateTime.now().toString();
+        log.info("created: {}, {}", this.getClass().getSimpleName(), this);
     }
 
     @PreUpdate
     protected void onUpdate() {
         updated = LocalDateTime.now().toString();
+        log.info("updated: {}, {}", this.getClass().getSimpleName(), this);
     }
 
     public LocalDateTime getUpdatedTime() {
