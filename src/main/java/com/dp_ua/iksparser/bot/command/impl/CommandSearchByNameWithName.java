1ae1a0d9d50311b9bb9d81bfc7e08d2af6f56ac5
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-01-23 14:20:23 +0200

    [structure] add Commands: searchByName

---
+++ src/main/java/com/dp_ua/iksparser/bot/command/impl/CommandSearchByNameWithName.java
@@ -0,0 +1,40 @@
+package com.dp_ua.iksparser.bot.command.impl;
+
+import com.dp_ua.iksparser.bot.Icon;
+import com.dp_ua.iksparser.bot.abilities.CompetitionFacade;
+import com.dp_ua.iksparser.bot.command.BaseCommand;
+import com.dp_ua.iksparser.bot.message.Message;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@ToString
+public class CommandSearchByNameWithName extends BaseCommand {
+    private final static String command = "searchbynamewithname";
+    private final boolean isInTextCommand = false;
+    @Autowired
+    CompetitionFacade competitionFacade;
+
+    @Override
+    public String command() {
+        return command;
+    }
+
+    @Override
+    protected String getTextForCallBackAnswer(Message message) {
+        return Icon.INFO + " Шукаємо дані по атлету " + Icon.INFO;
+    }
+
+    @Override
+    protected void perform(Message message) {
+        String chatId = message.getChatId();
+        String commandArgument = getCommandArgumentString(message.getMessageText());
+        competitionFacade.searchingByName(chatId, commandArgument, message.getEditMessageId());
+    }
+
+    public static String getTextForState(String competitionId) {
+        return "/" + command + " {\"id\":\"" + competitionId + "\",\"name\":\"{}\"}";
+    }
+}
--- src/test/java/com/dp_ua/iksparser/bot/command/CommandProviderTest.java
+++ src/test/java/com/dp_ua/iksparser/bot/command/CommandProviderTest.java
@@ -35,6 +35,8 @@ public class CommandProviderTest {
                         competition
                         competitions
                         help
+                        searchbyname
+                        searchbynamewithname
                         start""",
                 actual);
     }
