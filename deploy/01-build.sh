1a1bfea7ca37629334def7aae29aeb4105c71e97
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-02-03 15:44:17 +0200

    [deploy] save build scripts

---
+++ deploy/01-build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "---------------------- Docker build image -------------------"
+
+# Выполняем команду docker build
+docker build -t challenge-parser:latest .
+
+# Проверяем код возврата
+if [ $? -eq 0 ]; then
+    echo "Docker build successful!"
+
+else
+    echo "Docker build failed!"
+    exit 1
+fi
+
+echo "---------------------- Docker Finish -------------------"
+
+echo "---------------------- Deploy docker compose file -------------------"
+
+target_directory="/home/dp_ua/bots/docker/iks-bot"
+
+echo "----------------------Deploy Start -------------------"
+rm -rf "$target_directory"
+if [ ! -d "$target_directory" ]; then
+	mkdir -p "$target_directory"
+fi
+
+# Копируем Docker Compose файл в другую директорию
+cp docker-compose.yml "$target_directory/"
+ls -la "$target_directory"
+
+echo "---------------------- Finish Deploy docker compose file -------------------"
