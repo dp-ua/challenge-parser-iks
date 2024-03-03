104d913086f0ef8938fc23c03ef76236eb108300
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-03-03 17:43:18 +0200

    [api][refactoring] fix bug with kicked message

--- src/main/java/com/dp_ua/iksparser/bot/message/ReceivedMessage.java
+++ src/main/java/com/dp_ua/iksparser/bot/message/ReceivedMessage.java
@@ -97,8 +97,8 @@ public class ReceivedMessage implements Message {
         if (update.hasCallbackQuery()) {
             return update.getCallbackQuery().getFrom();
         }
-        if (update.hasChatMember()) {
-            return update.getChatMember().getFrom();
+        if (update.hasMyChatMember()) {
+            return update.getMyChatMember().getFrom();
         }
         return null;
     }
