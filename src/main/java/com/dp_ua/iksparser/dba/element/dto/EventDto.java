6a8ba4fc6bfabf891e7a13a4fe9e4744a9062c38
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-04-25 21:03:37 +0300

    [rest][event] add Dto and update Service

---
+++ src/main/java/com/dp_ua/iksparser/dba/element/dto/EventDto.java
@@ -0,0 +1,22 @@
+package com.dp_ua.iksparser.dba.element.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+public class EventDto {
+    private String time;
+    private String eventName;
+    private String category;
+    private String round;
+    private String startListUrl;
+    private String resultUrl;
+    private List<Long> heats;
+}
