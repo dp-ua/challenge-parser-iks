dffe74a700ea9ad225f5b47f5c147a2c1aa81791
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-08-20 13:17:46 +0300

    [refactoring][heatLineService] remove comment with SQL request

--- src/main/java/com/dp_ua/iksparser/dba/repo/HeatLineRepo.java
+++ src/main/java/com/dp_ua/iksparser/dba/repo/HeatLineRepo.java
@@ -20,13 +20,6 @@ public interface HeatLineRepo extends CrudRepository<HeatLineEntity, Long> {
             "WHERE d.competition.id = :competitionId AND hc.id = :coachId")
     List<HeatLineEntity> getHeatLinesInCompetitionWhereCoachIs(@Param("competitionId") Long competitionId,
                                                                @Param("coachId") Long coachId);
-    // this request is equal to
-    // SELECT hl FROM day_entity d
-    //JOIN event_entity e ON e.day_id = d.id
-    //JOIN heat_entity h ON h.event_id = e.id
-    //JOIN heat_line_entity hl ON hl.heat_id = h.id
-    //JOIN heatline_coach hc ON hl.id = hc.heatline_id
-    //where competition_id = 23 AND coach_id = 125;
 
     @Query("SELECT hl FROM DayEntity d " +
             "JOIN d.events e " +
