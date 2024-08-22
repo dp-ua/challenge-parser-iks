aa768d124dadbef011796568c52fd2a01f59281b
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2024-08-22 17:09:15 +0300

    [responseContent] add some defaults methods to he basic class

--- src/main/java/com/dp_ua/iksparser/bot/abilities/response/ResponseContent.java
+++ src/main/java/com/dp_ua/iksparser/bot/abilities/response/ResponseContent.java
@@ -2,8 +2,30 @@ package com.dp_ua.iksparser.bot.abilities.response;
 
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 
+import java.util.Optional;
+
 public interface ResponseContent {
     String getMessageText(Object... args);
 
     InlineKeyboardMarkup getKeyboard(Object... args);
+
+    default void validate(Object object, String message) {
+        if (object == null) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    default Optional<String> getArgument(int index, Object... args) {
+        if (args.length > index) {
+            return (Optional<String>) args[index];
+        }
+        return Optional.empty();
+    }
+
+    default Optional<Object> getArgumentObject(int index, Object... args) {
+        if (args.length > index) {
+            return Optional.of(args[index]);
+        }
+        return Optional.empty();
+    }
 }
