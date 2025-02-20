c6b8fe254827f521adaf7edc124ee4d1bfb50ac8
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2025-02-20 15:02:07 +0200

    [monitor] add @LogRequestDetails annotation to getHeatLineInfo and getHeatLinesByIds methods

--- src/main/java/com/dp_ua/iksparser/api/controller/HeatLineController.java
+++ src/main/java/com/dp_ua/iksparser/api/controller/HeatLineController.java
@@ -1,48 +1,50 @@
 package com.dp_ua.iksparser.api.controller;
 
-import com.dp_ua.iksparser.dba.dto.HeatLineDto;
-import com.dp_ua.iksparser.dba.entity.HeatLineEntity;
-import com.dp_ua.iksparser.dba.service.HeatLineService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.transaction.Transactional;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import static com.dp_ua.iksparser.api.v1.Variables.API_V1_URI;
+import static com.dp_ua.iksparser.api.v1.Variables.HEAT_LINE_URI;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-import static com.dp_ua.iksparser.api.v1.Variables.API_V1_URI;
-import static com.dp_ua.iksparser.api.v1.Variables.HEAT_LINE_URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dp_ua.iksparser.dba.dto.HeatLineDto;
+import com.dp_ua.iksparser.dba.entity.HeatLineEntity;
+import com.dp_ua.iksparser.dba.service.HeatLineService;
+import com.dp_ua.iksparser.monitor.LogRequestDetails;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
 @RequestMapping(API_V1_URI + HEAT_LINE_URI)
 @Tag(name = "HeatLine Management")
 public class HeatLineController {
-    @Autowired
-    private HeatLineService heatLineService;
+    private final HeatLineService heatLineService;
+
+    public HeatLineController(HeatLineService heatLineService) {
+        this.heatLineService = heatLineService;
+    }
 
     @Operation(summary = "Get heatLine info by id",
             description = "Get heatLine info by id")
     @GetMapping("/{id}")
     @Transactional
+    @LogRequestDetails(parameters = {"id"})
     public ResponseEntity<HeatLineDto> getHeatLineInfo(
-            HttpServletRequest request,
             @Schema(description = "HeatLine id")
             @PathVariable Long id) {
-
-        log.info("URI: {}, id: {} Request from IP: {}, User-Agent: {}",
-                request.getRequestURI(),
-                id,
-                request.getRemoteAddr(),
-                request.getHeader("User-Agent"));
 
         Optional<HeatLineEntity> heatLine = heatLineService.findById(id);
 
@@ -56,14 +58,9 @@ public class HeatLineController {
             description = "Get heatLines by provided list of ids")
     @PostMapping("/list")
     @Transactional
+    @LogRequestDetails(parameters = {"ids"})
     public ResponseEntity<List<HeatLineDto>> getHeatLinesByIds(
-            HttpServletRequest request,
             @RequestBody List<Long> ids) {
-        log.info("URI: {}, ids: {} Request from IP: {}, User-Agent: {}",
-                request.getRequestURI(),
-                ids,
-                request.getRemoteAddr(),
-                request.getHeader("User-Agent"));
 
         if (Objects.isNull(ids) || ids.isEmpty()) {
             return ResponseEntity.badRequest().build();
@@ -73,7 +70,7 @@ public class HeatLineController {
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .map(heatLineService::convertToDto)
-                .collect(Collectors.toList());
+                .toList();
 
         return ResponseEntity.ok(heatLines);
     }
