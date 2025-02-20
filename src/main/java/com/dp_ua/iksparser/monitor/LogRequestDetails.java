82a87fc725767e866d92a52368dff14218686f82
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2025-02-20 12:41:48 +0200

    [monitor] added log annotation for requests. Implemented aspect

---
+++ src/main/java/com/dp_ua/iksparser/monitor/LogRequestDetails.java
@@ -0,0 +1,12 @@
+package com.dp_ua.iksparser.monitor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogRequestDetails {
+    String[] parameters() default {};
+}
