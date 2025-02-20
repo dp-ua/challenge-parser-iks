b781c1242e6c743a06d2dce11462ed4694d78ca9
Pavlo Reshetylo <pavel.reshetilo@gmail.com>
2025-02-20 12:42:41 +0200

    [monitor] use @LogRequestDetails in CoachController

--- src/main/java/com/dp_ua/iksparser/api/controller/CoachController.java
+++ src/main/java/com/dp_ua/iksparser/api/controller/CoachController.java
@@ -2,13 +2,12 @@ package com.dp_ua.iksparser.api.controller;
 
 import com.dp_ua.iksparser.dba.dto.CoachDto;
 import com.dp_ua.iksparser.dba.service.CoachService;
+import com.dp_ua.iksparser.monitor.LogRequestDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,23 +21,20 @@ import static com.dp_ua.iksparser.api.v1.Variables.*;
 @RequestMapping(API_V1_URI + COACH_URI)
 @Tag(name = "Coach Management")
 public class CoachController {
-    @Autowired
-    private CoachService coachService;
+    private final CoachService coachService;
+
+    public CoachController(CoachService coachService) {
+        this.coachService = coachService;
+    }
 
     @Operation(summary = "Get coach info by id",
             description = "Get coach info by id")
     @GetMapping("/{id}")
     @Transactional
+    @LogRequestDetails(parameters = {"id"})
     public ResponseEntity<CoachDto> getCoachInfo(
-            HttpServletRequest request,
             @Schema(description = "Coach id")
             @PathVariable Long id) {
-
-        log.info("URI: {}, id: {} Request from IP: {}, User-Agent: {}",
-                request.getRequestURI(),
-                id,
-                request.getRemoteAddr(),
-                request.getHeader("User-Agent"));
 
         CoachDto coach = coachService.getCoachDto(id);
         if (coach == null) {
@@ -51,14 +47,9 @@ public class CoachController {
             description = "Get coaches info by provided list of ids")
     @PostMapping("/list")
     @Transactional
+    @LogRequestDetails(parameters = {"ids"})
     public ResponseEntity<List<CoachDto>> getCoachesByIds(
-            HttpServletRequest request,
             @RequestBody List<Long> ids) {
-        log.info("URI: {}, ids: {} Request from IP: {}, User-Agent: {}",
-                request.getRequestURI(),
-                ids,
-                request.getRemoteAddr(),
-                request.getHeader("User-Agent"));
 
         return ResponseEntity.ok(coachService.getCoachesDtoList(ids));
     }
@@ -67,20 +58,14 @@ public class CoachController {
             description = "Get coach by name")
     @GetMapping()
     @Transactional
+    @LogRequestDetails(parameters = {"text", "page", "size"})
     public ResponseEntity<Page<CoachDto>> getCoachByName(
-            HttpServletRequest request,
             @Schema(description = "Page number for results pagination", defaultValue = "0")
             @RequestParam(defaultValue = "0") int page,
             @Schema(description = "Size of the page for results pagination", defaultValue = DEFAULT_PAGE_SIZE)
             @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int size,
             @Schema(description = "Coach name(or part) to search by. Case insensitive.")
             @RequestParam String text) {
-
-        log.info("URI: {}, text: {} Request from IP: {}, User-Agent: {}",
-                request.getRequestURI(),
-                text,
-                request.getRemoteAddr(),
-                request.getHeader("User-Agent"));
 
         return ResponseEntity.ok(coachService.getByNamePartialMatch(text, page, size));
     }
