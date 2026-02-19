cbdc32d97b67cad45f9832db55576fb1f1f5f5cc
Pavlo Reshetylo <pavlo.reshetylo@medecision.com>
2026-02-19 18:32:31 +0200

    [notification] fix config

--- src/main/java/com/dp_ua/iksparser/configuration/NotificationConfigProperties.java
+++ src/main/java/com/dp_ua/iksparser/configuration/NotificationConfigProperties.java
@@ -4,8 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 /**
  * Configuration properties for notification system.
@@ -15,49 +13,24 @@ import lombok.Setter;
 @Data
 public class NotificationConfigProperties {
 
-    /**
-     * Notification mode: LEGACY, AGGREGATED, or BOTH.
-     */
     private NotificationMode mode = NotificationMode.BOTH;
 
-    /**
-     * Aggregated notification settings.
-     */
     private AggregatedConfig aggregated = new AggregatedConfig();
 
-    @Getter
-    @Setter
+    private ProcessorConfig processor = new ProcessorConfig();
+
+    @Data
     public static class AggregatedConfig {
 
-        /**
-         * Whether aggregated notifications are enabled.
-         */
         private boolean enabled = true;
 
-        /**
-         * Maximum number of notifications to include in a single message.
-         */
-        private int maxNotificationsPerMessage = 50;
-
-        /**
-         * Number of days after which processed notifications are deleted.
-         */
         private int cleanupAfterDays = 7;
 
     }
 
-    /**
-     * Notification processor configuration.
-     */
-    private ProcessorConfig processor = new ProcessorConfig();
-
-    @Getter
-    @Setter
+    @Data
     public static class ProcessorConfig {
 
-        /**
-         * Cron pattern for notification processor.
-         */
         private String cronPattern = "0 15,45 6,10,14,18,22 * * *";
 
     }
@@ -69,14 +42,6 @@ public class NotificationConfigProperties {
     public boolean isAggregatedEnabled() {
         return (mode == NotificationMode.AGGREGATED || mode == NotificationMode.BOTH)
                 && aggregated.isEnabled();
-    }
-
-    public int getMaxNotificationsPerMessage() {
-        return aggregated.getMaxNotificationsPerMessage();
-    }
-
-    public int getCleanupAfterDays() {
-        return aggregated.getCleanupAfterDays();
     }
 
 }
